1493b7586daf36c10fd9dce39ba2d1befdb19369
muhammad.falihadib <muhammadfalihadib@gmail.com>
2021-11-23 19:04:04 +0700

    add UserService

---
+++ siretail/src/main/java/apap/tugasakhir/siretail/service/UserService.java
@@ -0,0 +1,15 @@
+package apap.tugasakhir.siretail.service;
+
+import apap.tugasakhir.siretail.model.UserModel;
+
+import java.util.List;
+
+public interface UserService {
+    UserModel addUser(UserModel user);
+    UserModel updateUser(UserModel user);
+    void deleteUser(UserModel user);
+//    List<UserModel> getListUser();
+//    UserModel getUserById(Long id);
+    String encrypt(String password);
+    Boolean isValidPassword(UserModel user, String password);
+}
