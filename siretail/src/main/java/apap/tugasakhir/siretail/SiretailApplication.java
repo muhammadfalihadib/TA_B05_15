06d3df371d907d7430e54d919c45c8861ba78873
muhammad.falihadib <muhammadfalihadib@gmail.com>
2021-11-23 18:11:35 +0700

    create project

---
+++ siretail/src/main/java/apap/tugasakhir/siretail/SiretailApplication.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.siretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiretailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiretailApplication.class, args);
+	}
+
+}
---
+++ siretail/src/test/java/apap/tugasakhir/siretail/SiretailApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.siretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiretailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
