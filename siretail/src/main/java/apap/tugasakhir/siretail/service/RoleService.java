d5fc3ea651d0b645b3558f461260b1e8fb338cf8
muhammad.falihadib <muhammadfalihadib@gmail.com>
2021-11-24 06:50:38 +0700

    add RoleService

---
+++ siretail/src/main/java/apap/tugasakhir/siretail/service/RoleService.java
@@ -0,0 +1,9 @@
+package apap.tugasakhir.siretail.service;
+
+import apap.tugasakhir.siretail.model.RoleModel;
+
+import java.util.List;
+
+public interface RoleService {
+    List<RoleModel> getListRole();
+}
