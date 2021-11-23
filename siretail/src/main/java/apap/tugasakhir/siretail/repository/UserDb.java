be634d92ccbbfef731b3790cb2e6788f8ec3023e
muhammad.falihadib <muhammadfalihadib@gmail.com>
2021-11-23 19:11:22 +0700

    add UserDb

---
+++ siretail/src/main/java/apap/tugasakhir/siretail/repository/UserDb.java
@@ -0,0 +1,12 @@
+package apap.tugasakhir.siretail.repository;
+
+import apap.tugasakhir.siretail.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDb extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findById(Long id);
+}
