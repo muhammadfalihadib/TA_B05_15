ddc543e963a6695a608fe8f3698a06591744cacc
muhammad.falihadib <muhammadfalihadib@gmail.com>
2021-11-24 07:50:11 +0700

    exclude securityConfiguration for a while

--- siretail/src/main/java/apap/tugasakhir/siretail/SiretailApplication.java
+++ siretail/src/main/java/apap/tugasakhir/siretail/SiretailApplication.java
@@ -2,8 +2,9 @@ package apap.tugasakhir.siretail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SiretailApplication {
 
 	public static void main(String[] args) {
