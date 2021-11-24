854411fbc102e9a3dde3e37d47a16553baac38e1
muhammad.falihadib <muhammadfalihadib@gmail.com>
2021-11-24 07:43:07 +0700

    add updateUserFormPage & updateUserSubmitPage method

--- siretail/src/main/java/apap/tugasakhir/siretail/controller/UserController.java
+++ siretail/src/main/java/apap/tugasakhir/siretail/controller/UserController.java
@@ -5,6 +5,7 @@ import apap.tugasakhir.siretail.model.UserModel;
 import apap.tugasakhir.siretail.service.RoleService;
 import apap.tugasakhir.siretail.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -41,5 +42,29 @@ public class UserController {
         userService.addUser(user);
         model.addAttribute("username", user.getUsername());
         return "add-user";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateUserFormPage(
+            @PathVariable Long id,
+            Model model
+    ){
+        UserModel user = userService.getUserById(id);
+        List<RoleModel> listRole = roleService.getListRole();
+        model.addAttribute("user", user);
+        model.addAttribute("listRole", listRole);
+        return "form-update-user";
+    }
+
+    @PostMapping("/update")
+    public String updateUserSubmitPage(
+            @ModelAttribute UserModel user,
+            Model model
+    ){
+        user.setPassword(userService.encrypt(user.getPassword()));
+        UserModel updatedUser = userService.updateUser(user);
+        userService.updateUser(user);
+        model.addAttribute("username", updatedUser.getUsername());
+        return "update-user";
     }
 }
