ccd1368a4dee4c733ee28565ec1d22ced5c81d4b
Ahmad Harori Zaki Ichsan <ahmadharorizaki@gmail.com>
2021-12-24 21:51:48 +0700

    fix bad response status message

--- siretail/src/main/java/apap/tugasakhir/siretail/restcontroller/CabangRestController.java
+++ siretail/src/main/java/apap/tugasakhir/siretail/restcontroller/CabangRestController.java
@@ -23,21 +23,27 @@ public class CabangRestController {
     @PostMapping(value = "/cabang")
     private BaseResponse<CabangModel> createCabang(@Valid @RequestBody CabangDetail cabang,
                                            BindingResult bindingResult){
+        BaseResponse<CabangModel> response = new BaseResponse<>();
         if(bindingResult.hasFieldErrors()){
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field"
             );
         } else{
-            CabangModel cabangModel = new CabangModel();
-            cabangModel.setStatus(0);
-            cabangModel.setNama(cabang.getNama());
-            cabangModel.setAlamat(cabang.getAlamat());
-            cabangModel.setUkuran(cabang.getUkuran());
-            cabangModel.setNoTelp(cabang.getNoTelp());
-            BaseResponse<CabangModel> response = new BaseResponse<>();
-            response.setStatus(200);
-            response.setMessage("success");
-            response.setResult(cabangRestService.saveCabang(cabangModel));
+            try {
+                CabangModel cabangModel = new CabangModel();
+                cabangModel.setStatus(0);
+                cabangModel.setNama(cabang.getNama());
+                cabangModel.setAlamat(cabang.getAlamat());
+                cabangModel.setUkuran(cabang.getUkuran());
+                cabangModel.setNoTelp(cabang.getNoTelp());
+                response.setStatus(200);
+                response.setMessage("success");
+                response.setResult(cabangRestService.saveCabang(cabangModel));
+            } catch (Exception e){
+                response.setStatus(400);
+                response.setMessage("Request body has invalid type or missing field");
+                response.setResult(null);
+            }
             return response;
         }
     }
--- siretail/src/main/java/apap/tugasakhir/siretail/restcontroller/ItemCabangRestController.java
+++
@@ -1,9 +0,0 @@
-package apap.tugasakhir.siretail.restcontroller;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/v2")
-public class ItemCabangRestController {
-    
-}
